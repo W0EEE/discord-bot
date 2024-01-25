ed769ce22b56d97400788ced01f1d4676d6973b1
Eric Johnson <micromashor@gmail.com>
2024-01-25 13:20:45 -0600

    remove extra comma

--- commands/fcc.ts
+++ commands/fcc.ts
@@ -73,7 +73,7 @@ export async function execute(interaction, ctx) {
     grant_date, expired_date, cancellation_date, effective_date, last_action_date,
     entity_type, entity_name, first_name, mi, last_name, suffix,
     street_address, city, state, zip_code, po_box, attention_line, frn, applicant_type_code,
-    operator_class, trustee_callsign, trustee_name,
+    operator_class, trustee_callsign, trustee_name
     from l_HD JOIN l_EN USING(unique_system_identifier) JOIN l_AM using(unique_system_identifier)
     where l_HD.call_sign = $1::text ORDER BY last_action_date DESC LIMIT 1;`, [call]);
 
