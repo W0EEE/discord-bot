c4e5cd7fc4ec38fccd08fd1a02170dd7767c96cb
Eric Johnson <micromashor@gmail.com>
2024-06-28 00:59:21 -0400

    use effective_date to sort records (fixes #4)

--- db/index.ts
+++ db/index.ts
@@ -53,7 +53,7 @@ export async function licenseByCallsign(callsign: string): Promise<AmateurLicens
     street_address, city, state, zip_code, po_box, attention_line, frn, applicant_type_code,
     callsign, operator_class, trustee_callsign, trustee_name
     from l_HD JOIN l_EN USING(unique_system_identifier) JOIN l_AM using(unique_system_identifier)
-    where l_HD.call_sign = $1::text ORDER BY to_date(last_action_date, 'MM/DD/YYYY') DESC LIMIT 1;`, [callsign.trim().toUpperCase()]);
+    where l_HD.call_sign = $1::text ORDER BY to_date(effective_date, 'MM/DD/YYYY') DESC LIMIT 1;`, [callsign.trim().toUpperCase()]);
 
     const [record] = result.rows;
 
